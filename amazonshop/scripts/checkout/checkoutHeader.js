0c15dfe93f446bbb9c3fe3d56e40aa113c3f9390
wenbo2978 <161510538+wenbo2978@users.noreply.github.com>
2024-05-02 15:58:26 -0400

    Add files via upload
    add renderCheckoutHeader function

---
+++ amazonshop/scripts/checkout/checkoutHeader.js
@@ -0,0 +1,26 @@
+import { cartQuantity } from "../../data/cart.js";
+
+
+export function renderCheckoutHeader(){
+  let html = '';
+
+  html += `
+      <div class="checkout-header-left-section">
+        <a href="amazon.html">
+          <img class="amazon-logo" src="images/amazon-logo.png">
+          <img class="amazon-mobile-logo" src="images/amazon-mobile-logo.png">
+        </a>
+      </div>
+
+      <div class="checkout-header-middle-section">
+        Checkout (<a class="return-to-home-link js-checkout-item-quantity"
+          href="amazon.html">${cartQuantity} items</a>)
+      </div>
+
+      <div class="checkout-header-right-section">
+        <img src="images/icons/checkout-lock-icon.png">
+      </div>
+  `;
+
+  document.querySelector('.header-content').innerHTML = html;
+}
