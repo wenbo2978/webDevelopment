0bfd1a7a59d04f893fcb637b45c8104da38d364b
wenbo2978 <161510538+wenbo2978@users.noreply.github.com>
2024-06-16 23:42:05 -0400

    Add files via upload

---
+++ react-project1/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    //"src/components/navbar/index.jsx",
+    //"src/App.jsx",
+    //"src/**/*.{js, jsx, ts, tsx}",
+    "src/FoodRecipeApp/**/*.{jsx, js}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
